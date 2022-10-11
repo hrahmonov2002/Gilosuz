2c36f5a02292125058537a9954773641592f1b46
hrahmonov2002 <107402912+hrahmonov2002@users.noreply.github.com>
2022-10-11 15:28:48 +0500

    Add js code

--- js/data.js
+++ js/data.js
@@ -2,20 +2,29 @@ const products = [
   {
     id: 123,
     title: "Redmi Note 10 Pro",
-    img: "https://picsum.photos/300/200",
+    img: "https://picsum.photos/id/3/300/200",
     price: 4300000,
     model: "Xiaomi",
-    addedDate: new Date("2021-11-12").toISOString(),
+    addedDate: new Date("2021-11-12, 15:30").toISOString(),
     benefits: ["8gb", "128gb", "Waterproof"]
   },
   {
     id: 124,
-    title: "Samgung Note 20 Ultra",
-    img: "https://picsum.photos/300/200",
+    title: "Samsung Note 20 Ultra",
+    img: "https://picsum.photos/id/3/300/200",
     price: 8300000,
     model: "Samsung",
-    addedDate: new Date("2021-10-12").toISOString(),
+    addedDate: new Date("2021-10-12, 12:40").toISOString(),
     benefits: ["32gb", "1tb"]
+  },
+  {
+    id: 125,
+    title: "Iphone 12 Pro",
+    img: "https://picsum.photos/id/3/300/200",
+    price: 12300000,
+    model: "Apple",
+    addedDate: new Date("2021-10-12, 12:40").toISOString(),
+    benefits: ["1tb"]
   }
 ]
 
---
+++ test.js
@@ -0,0 +1,44 @@
+
+const names = [
+  {
+   name: "Mary",
+   height: 180,
+  },
+  {
+    name: "John",
+    height: 165
+  },
+  {
+    name: "Emma",
+    height: 170
+  }
+];
+
+// const arr = [];
+
+// const sortedNames = names.sort((a, b) => {
+//     if (b.height - a.height) {
+//     const elOutput = document.createElement("output");
+//     elOutput.textContent = names.name;
+//     } 
+           
+    
+   
+    
+    
+//     console.log(elOutput);
+// });
+
+
+// const names = ["Mary", "John", "Emma"]
+// const heights = [180, 165, 170];
+// let arr = [];
+
+// const sortedArr = heights.sort((a, b) => {
+//     if (b.heights - a.heights) {
+//         arr.push(names)
+//     } 
+// });
+
+// console.log(arr);
+
